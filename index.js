c1c5531ff0eb4719d124335aa754e6df348d6479
dianyi <lastobject@gmail.com>
2020-07-04 22:35:26 +1200

    move more varible to .env

--- index.js
+++ index.js
@@ -14,22 +14,31 @@ if (!fs.existsSync("nebula-cert")) {
   shell.exit(1);
 }
 
-const prefix = "NZ";
-const startIP = 100;
-const endIP = 255;
+const PREFIX = process.env.PREFIX;
+const BATCH_SIZE = process.env.BATCH_SIZE;
 const NEBULA_IP = process.env.NEBULA_IP;
 const PUBLIC_IP = process.env.PUBLIC_IP;
+const START_IP = Math.min(Math.max(process.env.START_IP || 2, 2), 254);
+const END_IP = Math.min(process.env.END_IP || 255, 255);
+const CONTAINS_EXEC_FILES = process.env.CONTAINS_EXEC_FILES == 0 ? false : true;
+
+console.log({ PREFIX, BATCH_SIZE, NEBULA_IP, PUBLIC_IP, START_IP, END_IP, CONTAINS_EXEC_FILES });
+
 const configPath = "config.yaml";
 
 const nebula_files = () => {
-  const nebulas = ["nebula", "ca.crt"];
-  const nebula_exe = "nebula.exe";
-  if (fs.existsSync(nebula_exe)) nebulas.push(nebula_exe);
+  const nebulas = ["ca.crt"];
+  if (CONTAINS_EXEC_FILES) {
+    const nebula_nix = "nebula";
+    const nebula_win = "nebula.exe";
+    if (fs.existsSync(nebula_nix)) nebulas.push(nebula_nix);
+    if (fs.existsSync(nebula_win)) nebulas.push(nebula_win);
+  }
   return nebulas;
 };
 
-for (let i = startIP; i < endIP; i++) {
-  const currentPath = `${prefix}_${i}`;
+for (let i = START_IP, j = 0; i < END_IP; i++) {
+  const currentPath = `${PREFIX}_${i}`;
   const pathExists = fs.existsSync(currentPath);
   const zipExists = fs.existsSync(currentPath + ".zip");
 
@@ -37,7 +46,6 @@ for (let i = startIP; i < endIP; i++) {
 
   // crt and key
   const ip = `${NEBULA_IP.split(".").slice(0, -1).join(".")}.${i}/24`;
-  console.log(ip);
   const sign_new = `./nebula-cert sign -name "${currentPath}" -ip "${ip}"`;
   if (shell.exec(sign_new).code !== 0) {
     shell.echo("ERROR: " + sign_new);
@@ -65,7 +73,8 @@ for (let i = startIP; i < endIP; i++) {
   // zip folder and remove
   shell.cd("..");
   shell.exec(`zip -vr ${currentPath}.zip ${currentPath}/ -x "*.DS_Store"`);
-  // shell.rm("-rf", currentPath);
+  shell.rm("-rf", currentPath);
+  console.log(ip);
 
-  return;
+  if (++j >= BATCH_SIZE) return;
 }
